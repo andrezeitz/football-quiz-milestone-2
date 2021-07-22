770500c0eed760abc8c221e48a536608bfaa4a01
andrezeitz <swe_zeitz@hotmail.com>
2021-07-22 17:57:48 +0000

    Add code to show questions

---
+++ assets/scripts/questions.js
@@ -0,0 +1,13 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who Win?",
+        answer: "a",
+        options: [
+            "a",
+            "s",
+            "d",
+            "f"
+        ]
+    }
+];
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-// all required elements
+// all required elements to open and close all boxes and be able to press on buttons
 const startBtn = document.querySelector(".start-btn button");
 const ruleBox = document.querySelector(".rule-box");
 const exitBtn = ruleBox.querySelector(".buttons .exit");
@@ -7,16 +7,25 @@ const quizBox = document.querySelector(".quiz-box");
 
 //If start
 startBtn.onclick = ()=> {
-    ruleBox.classList.add("activeInfo");
+    ruleBox.classList.add("activeInfo"); //show info box
 }
 
 //If exit
 exitBtn.onclick = ()=> {
-    ruleBox.classList.remove("activeInfo");
+    ruleBox.classList.remove("activeInfo"); //info box hidden
 }
 
 //If continue
 continueBtn.onclick = ()=> {
-    ruleBox.classList.remove("activeInfo");
-    quizBox.classList.add("activeQuiz");
+    ruleBox.classList.remove("activeInfo"); //rule box hidden
+    quizBox.classList.add("activeQuiz"); //show quiz box 
+    showQuestions();
+}
+
+let queCount = 0;
+
+function showQuestions(){
+    let questionText = document.querySelector(".question-text");
+    let queTag = '<span>'+ questions[0].question +'</span>';
+    questionText.innerHTML = queTag;
 }
